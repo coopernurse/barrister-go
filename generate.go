3e8d72408a633cb48501610c4c17cedfd54fd7e9
James Cooper <james@bitmechanic.com>
2012-08-26 16:37:11 -0700

    gofmt

--- generate.go
+++ generate.go
@@ -9,9 +9,9 @@ import (
 
 var reservedWords []string = []string{
 	"break", "default", "func", "interface", "select",
-    "case", "defer", "go", "map", "struct",
-    "chan", "else", "goto", "package", "switch",
-    "const", "fallthrough", "if", "range", "type",
+	"case", "defer", "go", "map", "struct",
+	"chan", "else", "goto", "package", "switch",
+	"const", "fallthrough", "if", "range", "type",
 	"continue", "for", "import", "return", "var",
 }
 
