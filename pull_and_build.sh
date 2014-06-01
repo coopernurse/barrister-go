79122f466653d3d9793cd05aad4a288f86aa531c
James Cooper <jamespcooper@gmail.com>
2014-05-31 22:00:19 -0700

    correct the path to barrister-go

--- pull_and_build.sh
+++ pull_and_build.sh
@@ -6,9 +6,9 @@ export GOROOT=/usr/local/go
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOROOT/bin
 
-mkdir -p $GOPATH/github.com/coopernurse
-rm -f $GOPATH/github.com/coopernurse/barrister-go
-ln -s `pwd` $GOPATH/github.com/coopernurse/barrister-go
+mkdir -p $GOPATH/src/github.com/coopernurse
+rm -f $GOPATH/src/github.com/coopernurse/barrister-go
+ln -s `pwd` $GOPATH/src/github.com/coopernurse/barrister-go
 
 go get -u github.com/couchbaselabs/go.assert
 
