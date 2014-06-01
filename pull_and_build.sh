86f01047c4aa030adc783ea37287f171760268de
James Cooper <jamespcooper@gmail.com>
2014-05-31 21:54:15 -0700

    set GOPATH and fetch go.assert

--- pull_and_build.sh
+++ pull_and_build.sh
@@ -3,7 +3,11 @@
 set -e
 
 export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
 export PATH=$PATH:$GOROOT/bin
+
+mkdir -p $GOPATH
+go get -u github.com/couchbaselabs/go.assert
 
 git pull
 ./test.sh
