5c198708c81cca86f25f79b32a951cf7f59468d1
James Cooper <jamespcooper@gmail.com>
2014-05-31 21:58:09 -0700

    sym link barrister-go src dir into GOPATH

--- pull_and_build.sh
+++ pull_and_build.sh
@@ -6,7 +6,10 @@ export GOROOT=/usr/local/go
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOROOT/bin
 
-mkdir -p $GOPATH
+mkdir -p $GOPATH/github.com/coopernurse
+rm -f $GOPATH/github.com/coopernurse/barrister-go
+ln -s `pwd` $GOPATH/github.com/coopernurse/barrister-go
+
 go get -u github.com/couchbaselabs/go.assert
 
 git pull
